936bb26eeb1383c6e74fea4ee7d19064660f60dc
villa718 <sebasvilla718@gmail.com>
2024-06-15 16:04:34 -0500

    calculadora

--- scripts.js
+++ scripts.js
@@ -27,11 +27,12 @@ function sumar() {
                                                                   }
 
                                                                   function potencia() {
-                                                                      // Ingresa aquí tu código.
-                                                                          // Ingresa aquí tu código.
-                                                                              // Ingresa aquí tu código. ¡¡¡Pista!!!, así se realiza la potencia: Math.pow(num1, num2);
-                                                                                  document.getElementById('resultado').innerText = resultado;
-                                                                                  }
+                                                                var num1 =parseFloat(document.getElementById('num1').value);
+                                                                var num2 =parseFloat(document.getElementById('num2').value);
+                                                                 var resultado = num1 ^ num2;
+                                                                            
+                                                                 document.getElementById('resultado').innerText = resultado;
+                                                                     }
 
 
                                                                                   function raiz() {
